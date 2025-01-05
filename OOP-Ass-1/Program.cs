9a44b0a47e9aa8f56708558cd887555feaa03393
khaled elbhdaly <155319819+kelbhdaly@users.noreply.github.com>
2025-01-05 21:22:11 +0200

    Q01

---
+++ OOP-Ass-1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+
+namespace OOP_Ass_1
+{
+
+
+    #region Q01
+    //class Point
+    //{
+    //    double x;
+    //    double y;
+
+    //    public double X { get => x; set => x = value; }
+    //    public double Y { get => y; set => y = value; }
+
+
+
+    //    public override string ToString()
+    //    {
+    //        return $"({x} , {y})";
+    //    }
+
+
+    //    static public Point CreatePoint()
+    //    {
+    //        double x1, y1;
+    //        bool flag01, flag02;
+    //        do
+    //        {
+    //            Console.WriteLine("Enter X1 Of Point ");
+    //            flag01 = double.TryParse(Console.ReadLine(), out x1);
+
+    //            Console.WriteLine("Enter Y1 Of Point ");
+    //            flag02 = double.TryParse(Console.ReadLine(), out y1);
+    //        } while (!(flag01 && flag02));
+
+    //        Point p01 = new Point();
+    //        p01.X = x1;
+    //        p01.Y = y1;
+
+
+    //        return p01;
+    //    }
+    //    public static double CalcDistance(Point p01, Point p02)
+    //    {
+    //        double distance = 0;
+    //        distance = (Math.Sqrt(Math.Pow(p01.X - p02.X, 2)) + Math.Sqrt(Math.Pow(p01.Y - p02.Y, 2)));
+    //        return distance;
+    //    }
+    //}
+    #endregion
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Q01
+            //Create a struct called "Point" to represent a 2D point with properties "X" and "Y".
+            //Write a C# program that takes two points as input from the user and calculates the distance between them.
+
+            //Point p01 = Point.CreatePoint();
+            //Point p02 = Point.CreatePoint();
+            //Console.WriteLine("The Distance Between = "+Point.CalcDistance(p01 , p02));
+
+            #endregion
+        }
+    }
+}
